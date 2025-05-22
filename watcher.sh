3932d6be43ff89463e872578b4180519e562e1b9
Juan Pablo Prada Mejia <juanpabloprada02112000@gmail.com>
2025-05-22 10:11:38 +0200

    Feat: Watcher script launch organizers in background

--- watcher.sh
+++ watcher.sh
@@ -4,19 +4,25 @@ function _video_changes() {
 		--event modify,move,create,delete "$videoPath" \
 		| while read changed; do
 			echo "$changed"
-			_organize
+			_organize_videos
 		done
 }
-# function _download_changes(){
-#
-# }
+function _download_changes() {
+	source ./downloads/dowloadOrganizer.sh
+	inotifywait --monitor --format "%e %w%f" \
+		--event modify,move,create,delete "$downloadPath" \
+		| while read changed; do
+			echo "$changed"
+			_organize_downloads
+		done
+}
 function _detect_changes() {
 	if [[ -z "$(which inotifywait)" ]]; then
 		echo -e "Not installed\nInstall inotify-tools\nsudo pacman -Syu inotify-tools"
 		exit 1
 	fi
-	_video_changes
-	# _download_changes
+	_video_changes &
+	_download_changes &
 }
 
 function main() {
