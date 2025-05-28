a56751fa3499f4b20747dafb3da3b51bb73dd7d4
Juan Pablo Prada Mejia <juanpabloprada02112000@gmail.com>
2025-05-28 09:21:25 +0200

    Feat: Changing temporally theh watcher because not working       when files takes time to be created, it deplaces them       before they are complete

--- watcher.sh
+++ watcher.sh
@@ -1,28 +1,36 @@
 function _video_changes() {
+	# not working yet as expected
+	# source ./video/videoOrganizer.sh
+	# inotifywait --monitor --format "%e %w%f" \
+	# 	--event modify,move,create,delete "$videoPath" \
+	# 	| while read changed; do
+	# 		echo "$changed"
+	# 		_organize_videos
+	# 	done
 	source ./video/videoOrganizer.sh
-	inotifywait --monitor --format "%e %w%f" \
-		--event modify,move,create,delete "$videoPath" \
-		| while read changed; do
-			echo "$changed"
-			_organize_videos
-		done
+	./_organize_videos
 }
 function _download_changes() {
+	# not working yet as expected
+	# source ./downloads/dowloadOrganizer.sh
+	# inotifywait --monitor --format "%e %w%f" \
+	# 	--event modify,move,create,delete "$downloadPath" \
+	# 	| while read changed; do
+	# 		echo "$changed"
+	# 		_organize_downloads
+	# 	done
 	source ./downloads/dowloadOrganizer.sh
-	inotifywait --monitor --format "%e %w%f" \
-		--event modify,move,create,delete "$downloadPath" \
-		| while read changed; do
-			echo "$changed"
-			_organize_downloads
-		done
+	./_organize_downloads
 }
 function _detect_changes() {
-	if [[ -z "$(which inotifywait)" ]]; then
-		echo -e "Not installed\nInstall inotify-tools\nsudo pacman -Syu inotify-tools"
-		exit 1
-	fi
-	_video_changes &
-	_download_changes &
+	# if [[ -z "$(which inotifywait)" ]]; then
+	# 	echo -e "Not installed\nInstall inotify-tools\nsudo pacman -Syu inotify-tools"
+	# 	exit 1
+	# fi
+	# _video_changes &
+	# _download_changes &
+	_video_changes
+	_download_changes
 }
 
 function main() {
