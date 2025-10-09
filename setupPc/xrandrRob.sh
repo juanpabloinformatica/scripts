ef8181ca3b5ef26ea23b022e27886cb9efec05f8
Juan Pablo Prada Mejia <juanpabloprada02112000@gmail.com>
2025-10-09 20:26:10 +0200

    Style: Removing unecessary echo command.

--- setupPc/xrandrRob.sh
+++ setupPc/xrandrRob.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/bash
+set -x
 # variables=$(find $HOME/Documents -type f -name "variables.sh" | grep -Pi "\/scripts\/variables.sh$")
 # source ${variables}
 
 function _switch_monitors_position() {
 	echo "Setting pc for bros settup"
-	if echo "$(iwgetid -r)" ~= "$WIFI_ROB" \
-		&& [[ "$(xrandr --listmonitors \
-			| awk 'NR==1 {print $2}')" -eq 3 ]]; then
+	  if [[ "$(iwgetid -r)" =~ "$WIFI_ROB" ]] \
+		&& [[ "$(xrandr --listmonitors | awk 'NR==1 {print $2}')" -eq 3 ]]; then
 		pcScreen="$(xrandr --listmonitors | awk 'NR>1 && $1 ~ /0:/ {print $NF}')"
 		firstMonitor="$(xrandr --listmonitors | awk 'NR>1 && $1 ~ /1:/ {print $NF}')"
 		secondMonitor="$(xrandr --listmonitors | awk 'NR>1 && $1 ~ /2:/ {print $NF}')"
