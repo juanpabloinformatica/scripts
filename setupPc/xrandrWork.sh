18d77b19c7fc0441e5ab578bbd903a63f6172387
Juan Pablo Prada Mejia <juanpabloprada02112000@gmail.com>
2025-10-09 20:26:45 +0200

    Fix: Condition, now working. Before was not detecting correctly if at work.

--- setupPc/xrandrWork.sh
+++ setupPc/xrandrWork.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/bash
+set -x
 function _at_work() {
-	if echo "$(iwgetid -r)" ~= "$WIFI_WORK"; then
-		echo "true"
-	fi
+	[[ "$(iwgetid -r)" =~ "$WIFI_WORK" ]] \
+		&& return 0 \
+		|| return 1
 }
 function _turn_off_edp() {
-	at_work=$(_at_work)
-	if [[ $at_work =~ "true" ]]; then
-		pcScreen="$(xrandr --listmonitors | awk 'NR>1 && $1 ~ /0:/ {print $NF}')"
-		xrandr --output "$pcScreen" --off
-	else
-		echo "Not at work"
-	fi
+
+	_at_work \
+		&& pcScreen="$(xrandr --listmonitors | awk 'NR>1 && $1 ~ /0:/ {print $NF}')" \
+		&& xrandr --output "$pcScreen" --off \
+		&& return 0 \
+		|| echo "Not at work"
 }
 function _work_init() {
-	echo "Setting up work, if there"
-	_turn_off_edp
+	echo "Setting up work, if there" \
+		&& _turn_off_edp
 }
