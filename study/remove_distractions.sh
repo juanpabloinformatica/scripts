1b9721476fec6889ab0b385efabe994e1629670c
Juan Pablo Prada Mejia <juanpabloprada02112000@gmail.com>
2025-10-28 21:37:29 +0100

    Feat: Adding solution for ipv6
    Now working for both ip versions.

--- study/remove_distractions.sh
+++ study/remove_distractions.sh
@@ -1,18 +1,22 @@
 #! /usr/bin/bash
-domains=("www.youtube.com")
+domains=("www.youtube.com" "www.monkeytype.com")
 
 function get_ips() {
 	counter=$((0))
 	for domain in "${domains[@]}"; do
 		((counter++))
 		while IFS= read -r -d $'\n' ipt; do
-			ips+=("${ipt}")
+			ipsv4+=("${ipt}")
 		done < <(nslookup "${domain}" | awk '/Address.*1.*\./{print $2}')
+
+		while IFS= read -r -d $'\n' ipt; do
+			ipsv6+=("${ipt}")
+		done < <(nslookup "${domain}" | awk '/Address:.*:/{print $2}')
 	done
 }
 function block_ips() {
 	i=$((0))
-	for ipt in "${ips[@]}"; do
+	for ipt in "${ipsv4[@]}"; do
 		if [[ ! -z ${ipt} ]]; then
 			((i++))
 			printf "BLOCKING:\t%s\n" "${ipt}"
@@ -20,13 +24,22 @@ function block_ips() {
 			sudo iptables -A FORWARD -d "$ipt" -j DROP
 		fi
 	done
+	for ipt in "${ipsv6[@]}"; do
+		if [[ ! -z ${ipt} ]]; then
+			((i++))
+			printf "BLOCKING:\t%s\n" "${ipt}"
+			sudo ip6tables -A OUTPUT -d "$ipt" -j DROP
+			sudo ip6tables -A FORWARD -d "$ipt" -j DROP
+		fi
+	done
 	printf "\nips removed:%d\n" "$i"
 }
 
 function main() {
-	ips=()
-	get_ips "${ips[@]}"
-	block_ips "${ips[@]}"
+	ipsv4=()
+	ipsv6=()
+	get_ips
+	block_ips
 }
 
 main "$@"
--- study/reset_distractions.sh
+++ study/reset_distractions.sh
@@ -3,11 +3,13 @@ set -x
 
 function reset_distractions() {
 	iptables -S | grep DROP | awk '{$1="";print $0}' | xargs -L1 iptables -D
+	ip6tables -S | grep DROP | awk '{$1="";print $0}' | xargs -L1 ip6tables -D
 }
 function is_there_distractions() {
-	dis=$(iptables -S | grep -c DROP)
-	printf "%d" "${dis}"
-	((dis > 0)) \
+	disv4=$(iptables -S | grep -c DROP)
+	disv6=$(ip6tables -S | grep -c DROP)
+	printf "%d" "${disv4}"
+	((disv4 > 0 && disv6)) \
 		&& return 0 \
 		|| return 1
 }
